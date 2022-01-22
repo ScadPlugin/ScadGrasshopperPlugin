7b384e503162d1a9f326a7bb44b9f880ff1783d8
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-01-22 14:19:49 +0300

    Создания типа и параметра свойств элемента

---
+++ ScadGrasshopperPlugin/Components/GHParameters/GHDataTypes/Elements/ScadPropertiesType.cs
@@ -0,0 +1,50 @@
+﻿using GHPlugin.Scad.Core.Entities.Elements.Interface;
+using Grasshopper.Kernel.Types;
+
+namespace ScadGrasshopperPlugin.Components.GHParameters.GHDataTypes.Elements
+{
+    public class ScadPropertiesType : GH_Goo<IScadElementProperties>
+   {
+
+       private readonly IScadElementProperties _scadElementProperties;
+        public ScadPropertiesType()
+        {
+            
+        }
+
+        public ScadPropertiesType(IScadElementProperties properties)
+        {
+            _scadElementProperties = properties;
+            Value = _scadElementProperties;
+        }
+
+        public ScadPropertiesType(ScadPropertiesType scadPropertiesType)
+        {
+            Value = scadPropertiesType.Value;
+        }
+        public override IGH_Goo Duplicate()
+        {
+            return new ScadPropertiesType(this);
+        }
+
+        public override string ToString()
+        {
+            return $"S_Properties:\n  -Rigid: {_scadElementProperties.RigidCode}";
+        }
+
+        public override bool IsValid
+        {
+            get
+            {
+                if (_scadElementProperties == null)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+        public override string TypeName { get => "S_Properties"; }
+        public override string TypeDescription { get => "Properties for SCAD elements"; }
+    }
+}
---
+++ ScadGrasshopperPlugin/Components/GHParameters/ScadPropertiesParameter.cs
@@ -0,0 +1,20 @@
+﻿using Grasshopper.Kernel;
+using ScadGrasshopperPlugin.Components.GHParameters.GHDataTypes.Elements;
+using System;
+
+namespace ScadGrasshopperPlugin.Components.GHParameters
+{
+    public class ScadPropertiesParameter : GH_Param<ScadPropertiesType>
+    {
+        public ScadPropertiesParameter() : base(
+            "ScadProperties", 
+            "S_Pr", 
+            "Properties for SCAD elements",
+            "SCAD",  
+            "Primitive", GH_ParamAccess.item)
+        {
+        }
+
+        public override Guid ComponentGuid { get => new Guid("42F7359C-535D-418B-A84E-ADBB92D955A2"); }
+    }
+}
