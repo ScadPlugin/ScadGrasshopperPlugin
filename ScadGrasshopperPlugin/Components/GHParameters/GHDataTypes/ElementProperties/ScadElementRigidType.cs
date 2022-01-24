66b145a2036c90e4812cd88a0144df0254742ed3
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-01-24 20:35:04 +0300

    Добавление нового типа и параметра для GH ScadElementRigid

---
+++ ScadGrasshopperPlugin/Components/GHParameters/GHDataTypes/ElementProperties/ScadElementRigidType.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GHPlugin.Scad.Core.Entities.ElementProperties.Interfaces;
+using Grasshopper.Kernel.Types;
+
+namespace ScadGrasshopperPlugin.Components.GHParameters.GHDataTypes.ElementProperties
+{
+    public class ScadElementRigidType : GH_Goo<IElementRigid>
+    {
+        private readonly IElementRigid _scadElementRigid;
+
+        public ScadElementRigidType()
+        {
+        }
+
+        public ScadElementRigidType(IElementRigid elementRigid)
+        {
+            _scadElementRigid = elementRigid;
+            Value = _scadElementRigid;
+        }
+
+        public ScadElementRigidType(ScadElementRigidType scadElementRigidType)
+        {
+            Value = scadElementRigidType.Value;
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new ScadElementRigidType(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Rigid: {_scadElementRigid.Name}";
+        }
+
+        public override bool IsValid
+        {
+            get
+            {
+                if (_scadElementRigid == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+        public override string TypeName { get => "S_Rigid"; }
+        public override string TypeDescription { get => "Element rigid"; }
+    }
+}
---
+++ ScadGrasshopperPlugin/Components/GHParameters/ScadElementRigidParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using ScadGrasshopperPlugin.Components.GHParameters.GHDataTypes.ElementProperties;
+
+namespace ScadGrasshopperPlugin.Components.GHParameters
+{
+    public class ScadElementRigidParameter : GH_Param<ScadElementRigidType>
+    {
+        public ScadElementRigidParameter():base(
+            "ScadElemRigid", 
+            "S_ER", 
+            "SCAD a element rigid", 
+            "SCAD", 
+            "Primitive", 
+            GH_ParamAccess.item)
+        {
+            
+        }
+        
+        public override Guid ComponentGuid { get => new Guid("C85307EA-2A60-4CCF-9DC2-046AE6AE23BF"); }
+    }
+}
